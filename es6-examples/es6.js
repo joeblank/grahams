babe33c89642e5370dfabadf070492717bf615f3
Joe Blank <joe.blank@live.com>
2017-01-29 21:21:59 -0700

    created basic examples for some of the new features for es6

---
+++ es6-examples/es6.js
@@ -0,0 +1,71 @@
+const hello = 'hello world';
+// hello = 'goodbye';
+//console.log(hello);
+
+//Support for constants (also known as "immutable variables"), i.e., variables
+// which cannot be re-assigned new content.
+
+let hello2 = 'hello world'
+hello2 = 'goodbye'
+//console.log(hello2);
+
+let baseball = 'homerun';
+baseball = 'strikeout';
+//console.log(baseball);
+
+//Notice: this only makes the variable
+// itself immutable, not its assigned content (for instance, in case the content
+// is an object, this means the object itself can still be altered).
+
+const grapes = {
+  size: 'small',
+  tastey: 'sometimes'
+};
+grapes.tastey = 'most of the time'
+//console.log(grapes);
+
+//arrow functions
+const words = 'I\'m a fat arrow function';
+const fn = words => {
+  console.log(words);
+}
+//fn(words);
+
+//default parameter values
+const x = 3;
+const y = undefined;
+const z = 7;
+
+const fn2 = (x, y = 10, z = 10) => {
+  console.log(x + y + z);
+}
+//fn2(x,y,z);
+
+//spread operator
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [...arr1, 6, 7, 8, 9, 10];
+//console.log(arr2);
+
+//string interpolation
+const name = "Joe";
+const hobbies = ['stuff', 'more stuff'];
+//console.log('Hi, my name is ' + name + ', and I like to do ' + hobbies[0] + ' and then ' + hobbies[1] + '!');
+//console.log(`Hi, my name is ${name}, and I like to do ${hobbies[0]} and then ${hobbies[1]}!`);
+
+//property shorthand
+const myName = 'Steph Curry'
+const obj1 = { myName: myName};
+const obj2 = { myName }
+//console.log(obj1, obj2);
+
+//method properties
+const methodObj = {
+  fn: function() {
+    console.log('fn works');
+  },
+  fn2 () {
+    console.log('fn2 works');
+  }
+};
+// methodObj.fn();
+// methodObj.fn2();
