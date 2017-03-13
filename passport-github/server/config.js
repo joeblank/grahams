dd2c9d24df31416e0bc52f68441c497b9c71ee90
Joe Blank <joe.blank@live.com>
2017-03-13 16:16:53 -0600

    added auth using github api

---
+++ passport-github/server/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  PORT: 4545,
+  SECRET: 'thisisthesecretthatdoesntmakeanysense',
+  GITHUB: {
+    CLIENT_ID: 'cccf223f59df61ccb9d8',
+    CLIENT_SECRET: '1baf541e4963b1a7f3868edc942d1e62411decca',
+    CALLBACK_URL: 'http://localhost:4545/auth/callback'
+  }
+}
